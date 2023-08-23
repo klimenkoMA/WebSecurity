85605607e80c267d743cf1bb9f30ef1e863e4bdc
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2023-08-23 19:42:02 +0300

    Added RegistrationController class

---
+++ security/src/main/java/com/weblessons/security/web/RegistrationController.java
@@ -0,0 +1,40 @@
+package com.weblessons.security.web;
+
+import com.weblessons.security.dto.UserRegistrationDTO;
+import com.weblessons.security.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registration")
+public class RegistrationController {
+
+    private UserService userService;
+
+    public RegistrationController(UserService userService) {
+        super();
+        this.userService = userService;
+    }
+
+    @ModelAttribute("user")
+    public UserRegistrationDTO userRegistrationDTO() {
+        return new UserRegistrationDTO();
+    }
+
+    @GetMapping
+    public String showRegistrationForm() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user")
+                                              UserRegistrationDTO registrationDTO) {
+        userService.save(registrationDTO);
+        return "redirect:/registration?success";
+    }
+
+
+}
